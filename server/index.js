0a0bcb4edb08777db77ea2ee863e336c0da2692b
jlg-repo <jlgbusiness@proton.me>
2026-05-22 16:58:49 -0400

    Railway deployment bugfix

--- server/index.js
+++ server/index.js
@@ -382,7 +382,7 @@ app.use((error, _req, res, _next) => {
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(join(__dirname, "../dist")));
-  app.get("*", (_req, res) => {
+  app.get("/{*splat}", (_req, res) => {
     res.sendFile(join(__dirname, "../dist/index.html"));
   });
 }
